2bf2f41d237c9d0306710f807b205079f71705ac
Jorge Ramirez <1074582@est.intec.edu.do>
2020-07-08 23:18:16 -0400

    Reverse LinkedList

--- DataStructures/LinkedList/LinkedList.js
+++ DataStructures/LinkedList/LinkedList.js
@@ -68,6 +68,25 @@ class LinkedList {
         }
         return currentNode;
     }
+    reverse() {
+        if (!this.head.next) {
+          return this.head;
+        }
+        let first = this.head;
+        this.tail = this.head;
+        let second = first.next;
+    
+        while(second) {
+          const temp = second.next;
+          second.next = first;
+          first = second;
+          second = temp;
+        }
+    
+        this.head.next = null;
+        this.head = first;
+        return this.printList();
+      }
 }
 
 const myLinkedList = new LinkedList(10);
